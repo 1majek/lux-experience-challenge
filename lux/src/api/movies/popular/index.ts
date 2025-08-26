424620bf92c8f1fd0178e2c74eda9948eb9fb7d7
Steven Majek <majek1@hotmail.com>
2025-08-26 20:50:02 +0200

    feat: add Movie interface to define movie data structure

--- lux/src/api/movies/popular/index.ts
+++ lux/src/api/movies/popular/index.ts
@@ -1,7 +1,9 @@
+import type { Movie } from "../interfaces"
+
 const API_KEY = import.meta.env.VITE_TMDB_API_KEY
 const BASE_URL = 'https://api.themoviedb.org/3'
 
-export const fetchPopularMovies = async () => {
+export const fetchPopularMovies = async (): Promise<Movie[]> => {
     const url = `${BASE_URL}/movie/popular?api_key=${API_KEY}&language=en-US&page=1`
     const response = await fetch(url)
     if (!response.ok) {
