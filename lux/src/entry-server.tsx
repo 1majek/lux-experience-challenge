c02f685a1ff65ac4e905b4fdbf83779d26253a58
Steven Majek <majek1@hotmail.com>
2025-08-27 21:48:42 +0200

    refactor: Handle routing with SSR

--- lux/src/entry-server.tsx
+++ lux/src/entry-server.tsx
@@ -1,13 +1,12 @@
-import { StrictMode } from 'react'
 import { renderToString } from 'react-dom/server'
 import App from './App'
+import { StaticRouter } from 'react-router-dom'
 
-
-export function render() {
+export function render(url: string) {
   const html = renderToString(
-    <StrictMode>
+    <StaticRouter location={url}>
       <App />
-    </StrictMode>,
+    </StaticRouter>,
   )
   return { html }
 }
