647048e49965d05ef64b3eabf2501d30133ffef6
Steven Majek <majek1@hotmail.com>
2025-08-30 13:07:11 +0200

    refactor: separate const and utils

--- lux/src/pages/home/useHome.ts
+++ lux/src/pages/home/useHome.ts
@@ -1,39 +1,39 @@
 import { usePopularMoviesApi } from "../../api/movies/popular/usePopularMoviesApi";
 import { useTopRatedMoviesApi } from "../../api/movies/top-rated/useTopRatedMoviesApi";
 import { useUpcomingMoviesApi } from "../../api/movies/upcoming/useUpcomingMoviesApi";
-import { popularMoviesTitle, topRatedMoviesTitle, upcomingMoviesTitle } from "../../utils";
+import { popularMoviesTitle, topRatedMoviesTitle, upcomingMoviesTitle } from "../../const";
 
 export const useHome = () => {
-    const popular = usePopularMoviesApi();
-    const topRated = useTopRatedMoviesApi();
-    const upcoming = useUpcomingMoviesApi();
+  const popular = usePopularMoviesApi();
+  const topRated = useTopRatedMoviesApi();
+  const upcoming = useUpcomingMoviesApi();
 
-    const carousels = [
-        {
-            title: popularMoviesTitle,
-            movies: popular.popularMovies,
-            isLoading: popular.isPopularMoviesLoading,
-            isError: popular.isPopularMoviesError,
-            categoryColor: "yellow"
-        },
-        {
-            title: topRatedMoviesTitle,
-            movies: topRated.topRatedMovies,
-            isLoading: topRated.isTopRatedMoviesLoading,
-            isError: topRated.isTopRatedMoviesError,
-            categoryColor: "magenta"
-        },
-        {
-            title: upcomingMoviesTitle,
-            movies: upcoming.upcomingMovies,
-            isLoading: upcoming.isUpcomingMoviesLoading,
-            isError: upcoming.isUpcomingMoviesError,
-            categoryColor: "pink"
-        },
-    ];
+  const carousels = [
+    {
+      title: popularMoviesTitle,
+      movies: popular.popularMovies,
+      isLoading: popular.isPopularMoviesLoading,
+      isError: popular.isPopularMoviesError,
+      categoryColor: "yellow"
+    },
+    {
+      title: topRatedMoviesTitle,
+      movies: topRated.topRatedMovies,
+      isLoading: topRated.isTopRatedMoviesLoading,
+      isError: topRated.isTopRatedMoviesError,
+      categoryColor: "magenta"
+    },
+    {
+      title: upcomingMoviesTitle,
+      movies: upcoming.upcomingMovies,
+      isLoading: upcoming.isUpcomingMoviesLoading,
+      isError: upcoming.isUpcomingMoviesError,
+      categoryColor: "pink"
+    },
+  ];
 
-    return {
-        carousels
-    }
+  return {
+    carousels
+  }
 
 }
--- lux/src/utils/index.ts
+++ lux/src/utils/index.ts
@@ -1,7 +1,3 @@
-export const popularMoviesTitle = "Popular Movies";
-export const topRatedMoviesTitle = "Top Rated Movies";
-export const upcomingMoviesTitle = "Upcoming Movies";
-
 export const getImageUrl = (path: string, size: string = "original") => {
   return `https://image.tmdb.org/t/p/${size}${path}`;
 }
